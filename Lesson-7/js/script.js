977bab0e81f279aec7d5ba8f1f82c4c34a3c7f94
tyko1986 <del21024@byui.edu>
2022-02-17 17:15:51 -0600

    commit

---
+++ Lesson-7/js/script.js
@@ -0,0 +1,36 @@
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+  rootMargin: '0px 0px 300px 0px',
+  threshold: 1, //.5
+};
+
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+
+if('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if(item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+
+
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+
