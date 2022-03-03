7360f91b588e0f4921d8e140729836c16b89340f
tyko1986 <del21024@byui.edu>
2022-03-03 12:57:52 -0600

    ordinary numbers

--- Lesson-9/js/prophets.js
+++ Lesson-9/js/prophets.js
@@ -24,10 +24,26 @@ function displayProphets(prophet) {
     h2.textContent = prophet.name + ' ' + prophet.lastname;
     birthDate.textContent = "Birth Date: " + prophet.birthdate;
     placeBirth.textContent = "Place of Birth: " + prophet.birthplace;
-    death.textContent = "Date of Death: " + prophet.death; 
+    death.textContent = "Date of Death: " + prophet.death;
+    
+    let orderPlace = '';
+
+
+    if (prophet.order === 1) {
+      orderPlace = prophet.order + 'st'
+    } else if (prophet.order === 2) {
+      orderPlace = prophet.order + 'nd'
+    } else if (prophet.order === 3) {
+      orderPlace = prophet.order + 'rd'
+    } else {
+      orderPlace = prophet.order + 'th'
+    }
+
+
+ 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
+    portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname + ',' + orderPlace + ' ' + 'President of the Church');
     portrait.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
@@ -36,6 +52,7 @@ function displayProphets(prophet) {
     card.appendChild(placeBirth);
     card.appendChild(death);
     card.appendChild(portrait);
+    
   
     // Add/append the existing HTML div with the cards class with the section(card)
     cards.appendChild(card);
