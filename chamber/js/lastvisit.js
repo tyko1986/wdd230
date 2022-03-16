35d0e9e7bae8520aea3bde4cb8db0513f95b7656
tyko1986 <del21024@byui.edu>
2022-03-15 19:35:59 -0500

    visits count

--- chamber/js/lastvisit.js
+++ chamber/js/lastvisit.js
@@ -1,5 +1,7 @@
-const daysDisplay = document.querySelector('.lastVisit');
-let lastvisit = window.localStorage.getItem('lastvisit-ls');
+
+let daysDisplay = document.querySelector('.lastVisit');
+let lastvisit = (window.localStorage.getItem('lastvisit-ls'));
+
 const FACTOR = 1000 * 60 * 60 * 24;
 let daysbetween = Date.now() - lastvisit;
 let numOfDays = Math.round(daysbetween / FACTOR);
@@ -9,7 +11,18 @@ if (numOfDays < 1){
 } else {
     daysDisplay.textContent = `${numOfDays} days since last visit`;
 }
-
-
 localStorage.setItem('lastvisit-ls', Date.now());
 
+
+
+let visitcount = document.querySelector('.visitcount');
+let numvisit = (window.localStorage.getItem('num-visits'));
+
+if (numvisit !== null) {
+    visitcount.textContent = `Visitor Number: ${numvisit}`;
+} else {
+    visitcount.textContent = `This is your First Visit`;
+}
+
+numvisit ++;
+localStorage.setItem("num-visits",numvisit);
