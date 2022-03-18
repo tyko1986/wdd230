9e803b9d288ef195f2e7591af42267fd4f19ba19
tyko1986 <del21024@byui.edu>
2022-03-17 23:57:18 -0500

    spotlights

--- chamber/js/spotlights.js
+++ chamber/js/spotlights.js
@@ -1,5 +1,7 @@
 const requestURL = "https://tyko1986.github.io/wdd230/chamber/JSON/directory.json";
-const cards = document.querySelector('#cards');
+const spot1 = document.querySelector('#sl1');
+const spot2 = document.querySelector('#sl2');
+const spot3 = document.querySelector('#sl3');
 
 fetch(requestURL)
   .then(function (response) {
@@ -9,10 +11,19 @@ fetch(requestURL)
     const directory = jsonObject['directory'];
     const goldMembers = directory.filter(x => (x.membership === "silver"));
 
-    goldMembers.forEach(displayGoldenMembers);
+    let randomMember1 = goldMembers[Math.floor(Math.random()*goldMembers.length)];
+    let randomMember2 = goldMembers[Math.floor(Math.random()*goldMembers.length)];
+    let randomMember3 = goldMembers[Math.floor(Math.random()*goldMembers.length)];
+    //goldMembers.forEach(displayGoldenMembers);
+
+    displayGoldMembers(randomMember1,spot1);
+    displayGoldMembers(randomMember2,spot2);
+    displayGoldMembers(randomMember3,spot3);
+
+
 });
 
-function displayGoldenMembers(company){
+function displayGoldMembers(randomMember,spot){
 
     let card = document.createElement('section');
     let name = document.createElement('h3');
@@ -21,21 +32,21 @@ function displayGoldenMembers(company){
     let website = document.createElement('p');
     let logo = document.createElement('img');
 
-    name.textContent = company.name;
-    address.textContent = company.address;
-    phone.textContent = company.phone;
-    website.textContent = company.website;
+    name.textContent = randomMember.name;
+    address.textContent = randomMember.address;
+    phone.textContent = randomMember.phone;
+    website.textContent = randomMember.website;
 
-    
-    logo.setAttribute('src', company.logo);
+
+    logo.setAttribute('src', randomMember.logo);
     logo.setAttribute('class', 'directoryLogo')
-    logo.setAttribute('alt', company.name + 'logo');
+    logo.setAttribute('alt', randomMember.name + 'logo');
     logo.setAttribute('loading', 'lazy');
     name.setAttribute('class','directoryInfo');    
     address.setAttribute('class','directoryInfo');
     phone.setAttribute('class','directoryInfo');    
     website.setAttribute('class','directoryInfo');
-    
+
 
     card.appendChild(logo);
     card.appendChild(name);
@@ -43,5 +54,5 @@ function displayGoldenMembers(company){
     card.appendChild(phone);
     card.appendChild(website);
 
-    cards.appendChild(card);
-}
+    spot.appendChild(card);
+}    
