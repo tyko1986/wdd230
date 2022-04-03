edc6c97bd508def50a0ee732c69ac87359fcf517
tyko1986 <del21024@byui.edu>
2022-04-02 20:56:42 -0500

    Json

--- temple/JS/script.js
+++ temple/JS/script.js
@@ -203,4 +203,71 @@ document.querySelector('#thirdDay').innerHTML = `${tomorrow3}`;
 
 /******** TEMPLES CARDS ***********/
 
-/*https://github.com/tyko1986/wdd230/blob/main/temple/JSON/temples.JSON*/
+const templeApi = "https://github.com/tyko1986/wdd230/blob/main/temple/JSON/temples.JSON";
+const cards = document.querySelector('.templeCard');
+
+fetch(templeApi)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const temples = jsonObject['temples'];
+    temples.forEach(displayTemples);
+});
+
+function displayTemples(){
+
+  // Create elemnts to add to the Card
+  let card = document.createElement('section');
+  let name = document.createElement('h2');
+  let address = document.createElement('p');
+  let phone = document.createElement('p');
+  let picture = document.createElement('img');
+  let email = document.createElement('p');
+  let services = document.createElement('p');
+  let announced = document.createElement('p');
+  let groundbreaking = document.createElement('p');
+  let dedicated = document.createElement('p');
+  let ordinances_week = document.createElement('p');
+  let ordinances_weekends = document.createElement('p');
+  let sessions_week = document.createElement('p');
+  let sessions_weekends = document.createElement('p');
+  let closure = document.createElement('p');
+
+  //Add the text content to each elemnt
+
+  name.textContent = temples.name;
+  address.textContent = temples.address;
+  phone.textContent = temples.phone;
+  picture.setAttribute('src',temples.picture);
+  picture.setAttribute('alt',temples.name);
+  picture.setAttribute('loading','lazy');
+  email.textContent = temples.email
+  services.textContent = temples.services;
+  announced.textContent = temples.history[0].announced;
+  groundbreaking.textContent = temples.history[0].groundbreaking;
+  dedicated.textContent = temples.history[0].dedicated;
+  ordinances_week.textContent = temples.ordinances_schedule[0].between_week;
+  ordinances_weekends.textContent = temples.ordinances_schedule[0].weekends;
+  sessions_week.textContent = temples.session_schedule[0].between_week;
+  sessions_weekends.textContent = temples.session_schedule[0].weekends;
+  closure.textContent = temples.closure_schedule;
+
+
+
+
+
+  
+
+
+
+  
+
+
+
+  
+
+
+
+  
+} 
